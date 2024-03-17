fa35362249437d54fdd9ef3859f7db37bcc6f647
Matheus Altrão <matheusaltrao2@gmail.com>
2024-03-17 18:27:25 -0400

    feat: add actions button in finance

--- src/app/finance/components/FinanceTable.tsx
+++ src/app/finance/components/FinanceTable.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import {
     Table,
     TableBody,
@@ -7,44 +8,100 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
+import { Eye, Trash } from 'lucide-react';
+import ActionsButtons from './ActionsButton';
+import Ball from './Ball';
 
 const FinanceTable = () => {
+
+    const object = [
+        {
+            name: 'Escola',
+            method: 'Dinheiro',
+            status: 'Saída',
+            category: 'Educação',
+            amount: '250,00',
+            date: '17/03/2024',
+            bank: 'Nubank'
+        },
+        {
+            name: 'Supermercado',
+            method: 'Cartão de Débito',
+            status: 'Saída',
+            category: 'Alimentação',
+            amount: '450,00',
+            date: '20/03/2024',
+            bank: 'Banco do Brasil'
+        },
+        {
+            name: 'Website',
+            method: 'Transferência Bancária',
+            status: 'Entrada',
+            category: 'Trabalho',
+            amount: '1200,00',
+            date: '25/03/2024',
+            bank: 'Caixa Econômica'
+        },
+        {
+            name: 'Assinatura Netflix',
+            method: 'Cartão de Crédito',
+            status: 'Saída',
+            category: 'Lazer',
+            amount: '45,90',
+            date: '01/03/2024',
+            bank: 'Santander'
+        },
+        {
+            name: 'Website',
+            method: 'Transferência Bancária',
+            status: 'Entrada',
+            category: 'Trabalho',
+            amount: '1200,00',
+            date: '25/03/2024',
+            bank: 'Caixa Econômica'
+        },
+        {
+            name: 'Mensalidade Academia',
+            method: 'Dinheiro',
+            status: 'Saída',
+            category: 'Saúde',
+            amount: '89,99',
+            date: '10/03/2024',
+            bank: 'Nubank'
+        },
+    ];
+
+
     return (
         <Table>
-
             <TableHeader>
                 <TableRow>
-                    <TableHead className='w-[100px]'>Invoice</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Method</TableHead>
-                    <TableHead className='text-right'>Amount</TableHead>
+                    <TableHead>Nome</TableHead>
+                    <TableHead>Método de Pagamento</TableHead>
+                    <TableHead>Categoria</TableHead>
+                    <TableHead>Valor</TableHead>
+                    <TableHead>Data</TableHead>
+                    <TableHead>Conta</TableHead>
+                    <TableHead className='text-right'>Ações</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow>
-                    <TableCell className='font-medium'>INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className='text-right'>$250.00</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell className='font-medium'>INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className='text-right'>$250.00</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell className='font-medium'>INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className='text-right'>$250.00</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell className='font-medium'>INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className='text-right'>$250.00</TableCell>
-                </TableRow>
+                {object.map((item, index) => (
+                    <TableRow key={index}>
+                        <TableCell className='font-medium   gap-1 relative pl-8'>
+                            <Ball status={item.status} />{item.name}</TableCell>
+                        <TableCell >{item.method}</TableCell>
+                        <TableCell>{item.category}</TableCell>
+                        <TableCell>{item.amount}</TableCell>
+                        <TableCell>{item.date}</TableCell>
+                        <TableCell>{item.bank}</TableCell>
+
+                        <TableCell className='text-right space-x-2' >
+                            <ActionsButtons />
+                        </TableCell>
+                    </TableRow>
+                ))}
+
             </TableBody>
         </Table>
     );
