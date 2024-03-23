52fb6b03325c87dc9b9d62980c790afc0bd61562
Matheus Altrão <matheusaltrao2@gmail.com>
2024-03-23 12:29:25 -0400

    feat: add auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import { AuthProvider } from '@/providers/auth';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,13 +26,15 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={inter.className}>
-                <ThemeProvider
-                    attribute='class'
-                    defaultTheme='light'
-                    disableTransitionOnChange
-                >
-                    {children}
-                </ThemeProvider>
+                <AuthProvider>
+                    <ThemeProvider
+                        attribute='class'
+                        defaultTheme='light'
+                        disableTransitionOnChange
+                    >
+                        {children}
+                    </ThemeProvider>
+                </AuthProvider>
             </body>
         </html>
     );
