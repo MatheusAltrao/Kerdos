2adc63872ba2623c3deea0ee2e42734f4ebc9117
Matheus Altrão <matheusaltrao2@gmail.com>
2024-03-23 12:31:41 -0400

    feat: add filter

--- src/app/home/finance/page.tsx
+++ src/app/home/finance/page.tsx
@@ -2,12 +2,27 @@ import Container from '@/components/content/Container';
 import Header from '@/components/content/Header';
 import Sidebar from '@/components/sidebar/Sidebar';
 import FinanceTable from './components/FinanceTable';
+import { Button } from '@/components/ui/button';
+import { Calendar, Plus } from 'lucide-react';
 
 const Finance = () => {
     return (
         <Header name='Finanças'>
             <Container>
-                <FinanceTable />
+                <div className='w-full space-y-8' >
+                    <header>
+                        <div className='flex items-center gap-4 justify-between' >
+                            <input type="text" placeholder='Pesquise aqui' className='h-10 bg-muted rounded focus:outline-none p-2 w-full ' />
+
+                            <div className='flex items-center gap-2' >
+                                <Button variant={'outline'} className='gap-2' ><Calendar size={20} />Data</Button>
+                                <Button variant={'outline'} className='gap-2' ><Plus size={20} /> Adicionar</Button>
+                            </div>
+
+                        </div>
+                    </header>
+                    <FinanceTable />
+                </div>
             </Container>
         </Header>
     );
