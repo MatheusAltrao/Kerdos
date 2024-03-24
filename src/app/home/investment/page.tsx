bb5de9d41b20a87bfc86717ceadb42e64ed99789
Luis Felipe <luisfelipesdn12@gmail.com>
2024-03-24 11:55:09 -0300

    feat: Add scrollarea in investiments

--- src/app/home/investment/page.tsx
+++ src/app/home/investment/page.tsx
@@ -118,8 +118,7 @@ const Investiment = async () => {
     return (
         <Header name='Investimento'>
             <Container>
-                <ScrollArea className="w-full flex flex-1 h-[90vh] pr-4  overflow-y-auto  " >
-
+                <ScrollArea className="w-full flex flex-1 h-[90vh] pr-4 pt-4 pb-12 overflow-y-auto  " >
                     <div className="w-full  space-y-8 " >
                         <div className="space-y-4" >
                             <h2 className="uppercase font-medium text-muted-foreground" >Moedas</h2>
