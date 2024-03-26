9c12595660b282053ca64b7c3557d380ae7a08f4
Matheus Altrão <matheusaltrao2@gmail.com>
2024-03-25 23:08:34 -0400

    fix: add date filter

--- src/app/home/dashboard/page.tsx
+++ src/app/home/dashboard/page.tsx
@@ -1,39 +1,103 @@
+'use client'
 import Container from '@/components/content/Container';
 import Header from '@/components/content/Header';
-import Sidebar from '@/components/sidebar/Sidebar';
 import Card from './components/Card';
-import { DollarSign } from 'lucide-react';
+import { CalendarIcon, DollarSign } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import { Calendar } from '@/components/ui/calendar';
+import { useState } from 'react';
+import useDateRange from '@/hooks/useDateRange';
 
 const Dashboard = () => {
+    const [startDate, setStartDate] = useState<Date | undefined>(new Date())
+    const [endDate, setEndDate] = useState<Date | undefined>(undefined);
+    useDateRange(startDate, endDate, setEndDate)
+
     return (
         <Header name='Dashboard'>
             <Container>
-                <div className='w-full'>
-                    <div className='grid grid-cols-4 w-full gap-4'>
+                <div className='w-full space-y-8'>
+
+                    <div className='flex items-center justify-end gap-4' >
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-[150px] justify-start text-left font-normal",
+                                        !startDate && "text-muted-foreground"
+                                    )}
+                                >
+                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                    {startDate ? format(startDate, "dd/MM/yyyy") : <span>Selecione uma data</span>}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0">
+                                <Calendar
+                                    required
+                                    mode="single"
+                                    showOutsideDays={false}
+                                    selected={startDate}
+                                    onSelect={setStartDate}
+                                    initialFocus
+                                />
+                            </PopoverContent>
+                        </Popover>
+                        -
+                        <Popover  >
+                            <PopoverTrigger asChild>
+                                <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-[150px] justify-start text-left font-normal",
+                                        !endDate && "text-muted-foreground"
+                                    )}
+                                >
+                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                    {endDate ? format(endDate, "dd/MM/yyyy") : <span>Escolha uma data</span>}
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-auto p-0">
+                                <Calendar
+                                    required
+                                    mode='single'
+                                    showOutsideDays={false}
+                                    selected={endDate}
+                                    onSelect={setEndDate}
+                                    className='rounded-md '
+                                    fromDate={startDate}
+                                />
+                            </PopoverContent>
+                        </Popover>
+                    </div>
+
+                    <div className='grid grid-cols-3 w-full gap-4'>
+
                         <Card
-                            amount={7200}
-                            description='Soma de todas as finanças feitas'
+                            amount={5000}
+                            title='Entradas'
+                            description='Soma de todas as entradas'
                             icon={<DollarSign size={16} />}
-                            title='Valor Total'
+
                         />
                         <Card
-                            amount={7200}
-                            description='Soma de todas as finanças feitas'
+                            amount={5000}
+                            title='Saídas'
+                            description='Soma de todas as saídas'
                             icon={<DollarSign size={16} />}
-                            title='Valor Total'
                         />
+
                         <Card
-                            amount={7200}
-                            description='Soma de todas as finanças feitas'
-                            icon={<DollarSign size={16} />}
+                            amount={10000}
                             title='Valor Total'
-                        />
-                        <Card
-                            amount={7200}
-                            description='Soma de todas as finanças feitas'
+                            description='Soma de todas as finanças'
                             icon={<DollarSign size={16} />}
-                            title='Valor Total'
+
                         />
+
                     </div>
                 </div>
             </Container>
