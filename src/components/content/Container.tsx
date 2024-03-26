17f278d1958c2a4aafc05a6482663f041a21a6bd
Matheus Altrão <matheusaltrao2@gmail.com>
2024-03-25 23:10:40 -0400

    fix: remove pt-4

--- src/components/content/Container.tsx
+++ src/components/content/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 
 const Container = ({ children, className }: ContainerProps) => {
     return <div className={`flex  flex-1 py-4 px-8 ${className}`}>
-        <ScrollArea className="w-full flex flex-1 h-[90vh] pr-4 pt-4 pb-12 overflow-y-auto  " >
+        <ScrollArea className="w-full flex flex-1 h-[90vh] pr-4  pb-12 overflow-y-auto  " >
             {children}
         </ScrollArea>
     </div>;
