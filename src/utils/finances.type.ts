dd097d5d8f90eb862db5a37948c5dcb82ed26531
Matheus Altrão <matheusaltrao2@gmail.com>
2024-03-25 23:11:18 -0400

    feat: add finance type

---
+++ src/utils/finances.type.ts
@@ -0,0 +1,11 @@
+export interface FinancesProps {
+    id: number,
+    name: string,
+    paymentMethod: string,
+    category: string,
+    amount: string,
+    date: Date,
+    bank: string,
+    transactionType: string,
+    userId: string,
+}
