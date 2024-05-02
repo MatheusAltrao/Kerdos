7dfae14b138f691987578770a56b0ebb5a7b18d8
Matheus Altrão <matheusaltrao2@gmail.com>
2024-05-02 15:22:56 -0400

    fix: auth name

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,26 @@
 import { prismaClient } from "@/lib/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { AuthOptions } from 'next-auth';
+import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
 
-    adapter: PrismaAdapter(prismaClient),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        })
-    ],
-    callbacks: {
-        async session({ session, user }) {
-            session.user = { ...session.user, id: user.id } as {
-                id: string;
-                name: string;
-                email: string;
-            };
-
-            return session;
-        },
+      return session;
     },
-    secret: process.env.NEXTAUTH_SECRET as string,
-
-}
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
+};
